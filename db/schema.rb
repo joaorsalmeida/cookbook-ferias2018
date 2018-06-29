8095d4e7ccf54ca3903330b69bfa09110e965be8
Joao Almeida <joaorsalmeida@gmail.com>
2018-06-28 21:54:40 -0300

    Visitante ve detalhes da receita

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_28_235456) do
+ActiveRecord::Schema.define(version: 2018_06_29_003519) do
 
   create_table "recipes", force: :cascade do |t|
     t.string "title"
@@ -20,6 +20,8 @@ ActiveRecord::Schema.define(version: 2018_06_28_235456) do
     t.integer "cook_time"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "ingredients"
+    t.text "cook_method"
   end
 
 end
